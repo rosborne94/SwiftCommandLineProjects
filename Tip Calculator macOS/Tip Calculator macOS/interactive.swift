4432a2edf273853f4546dcbdaa8abe2985f4a5c3
Riley Osborne <mitchell.osborne@interapt.hq>
2016-10-10 10:28:07 -0400

    added interactive file

---
+++ Tip Calculator macOS/Tip Calculator macOS/interactive.swift
@@ -0,0 +1,9 @@
+//
+//  interactive.swift
+//  Tip Calculator macOS
+//
+//  Created by Riley Osborne on 10/10/16.
+//  Copyright © 2016 Riley Osborne. All rights reserved.
+//
+
+import Foundation
---
+++ Tip Calculator macOS/Tip Calculator macOS/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tip Calculator macOS
+//
+//  Created by Riley Osborne on 10/10/16.
+//  Copyright © 2016 Riley Osborne. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+print("Poop")
